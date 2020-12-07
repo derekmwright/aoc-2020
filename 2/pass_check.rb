e3e23ef4eb6671486b42562e7f027561be8c345b
Derek Wright <dwright@eitccorp.com>
2020-12-07 10:20:22 -0500

    Day 2

---
+++ 2/pass_check.rb
@@ -0,0 +1,33 @@
+class Password
+  include Comparable
+
+  PASS_REGEX = /^(?<pos1>[\d]+)-(?<pos2>[\d]+)\s+(?<char>[\w]):\s(?<password>[\w]+)$/
+
+  attr_reader :pos1, :pos2, :char, :password
+
+  def initialize(line)
+    if matches = line.match(PASS_REGEX)
+      @pos1 = matches[:pos1].to_i
+      @pos2 = matches[:pos2].to_i
+      @char = matches[:char]
+      @password = matches[:password]
+      @valid = self.valid?
+    else
+      return false
+    end
+  end
+
+  def old_valid?
+    return @password.count(@char).between?(@pos1, @pos2)
+  end
+
+  def valid?
+    return ((@password[@pos1-1] <=> @char) + (@password[@pos2-1] <=> @char)).abs == 1
+  end
+end
+
+passwords = File.readlines('input.txt', chomp: true)
+passwords.map! { |p| Password.new(p) }
+
+p passwords.select { |p| p.old_valid? }.count
+p passwords.select { |p| p.valid? }.count
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 I got a late start on this and Ruby is my go-to language for get'er done.
 
 I'm thinking I could have used a single functions w/ some recursion but this completed the challenge and it isnt overly complex. To do this recursively I'd have to take into account the number of iterations and the challenge only went to 3, so meh.
+
+## Day 2
+
+Still catching up, still Ruby. Made a class for this one, I like it...
